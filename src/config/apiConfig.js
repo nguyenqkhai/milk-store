b1cec408caa01f49ea43d65ef93a4f3021cc3b8d
nguyenqkhai <nguyenkhai7803@gmail.com>
2025-05-17 20:37:06 +0700

    fix endpoint api

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,10 +2,10 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const apiConfig = {
   API_BASE_URL,
   AUTH: {
-    SEND_OTP: '/api/Auth/send-otp',
-    VERIFY_OTP: '/api/Auth/verify-otp',
-    CUSTOMER_LOGIN: '/api/Auth/customer/login',
-    REFRESH_TOKEN: '/api/Auth/refresh-token'
+    SEND_OTP: '/Auth/send-otp',
+    VERIFY_OTP: '/Auth/verify-otp',
+    CUSTOMER_LOGIN: '/Auth/customer/login',
+    REFRESH_TOKEN: '/Auth/refresh-token'
   }
 };
 
